c27f630423dad53ab6ff84be690aa12ba0dd3b3b
刘森华 <1790615567@qq.com>
2019-04-09 17:34:58 +0800

    test

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -8,7 +8,7 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         try {
-            response.getWriter().println("<h1>Hello Servlet<h1>");
+            response.getWriter().println("<h1>Hello World<h1>");
             response.getWriter().println(new Date().toString());
         } catch(IOException e) {
             e.printStackTrace();
